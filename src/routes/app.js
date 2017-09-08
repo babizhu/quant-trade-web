9b3c95e2776df20dbf0ea75a1dbc01cb5d0814ed
liulaoye <liu_kun@msn.com>
2017-09-08 15:22:53 +0800

    在小屏幕下放弃了点击空白处收起sidebar的功能，一来是这个功能实现的方案不合理，二来是点击空白处每次都要产生一个action，影响我现在开发

--- src/routes/app.js
+++ src/routes/app.js
@@ -82,7 +82,7 @@ const App = ({ children, dispatch, app, loading, location }) => {
         <aside className={styles.sidebar} style={{ position: sidebarPosition, display: displayMode, height }}>
           <Sidebar {...sidebarProps} />
         </aside>
-
+        <div className={styles.container} style={{ marginLeft: contentMarginLeft }}>
 
 
           <Breadcrumb {...breadcrumbProps} />
