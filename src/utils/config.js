7933ac07ecfc89d02f6b59ca1041941b19e12dd7
liulaoye <liu_kun@msn.com>
2017-09-08 10:17:20 +0800

    config.js文件还是不能不放一份在github上

---
+++ src/utils/config.js
@@ -0,0 +1,30 @@
+const APIV1 = '/api';
+const APIV2 = '/api/v2';
+const HOST = 'http://192.168.1.156:8080';
+export default {
+  host: HOST,
+  name: 'Quant Trade',
+  prefix: 'quantTrade',
+  // footerText: 'Ant Design Admin  © 2017 zuiidea',
+  logo: '/logo.png',
+  iconFontCSS: '/iconfont.css',
+  iconFontJS: '/iconfont.js',
+  YQL: ['http://www.zuimeitianqi.com'],
+  CORS: [],
+  // openPages: ['/login'],
+  apiPrefix: '/api/v1',
+  api: {
+    userLogin: `${APIV1}/login`,
+    userLogout: `${APIV1}/logout`,
+    userInfo: `${APIV1}/userInfo`,
+    users: `${APIV1}/users`,
+    userQuery: `${APIV1}/user/query`,
+    userSave: `${APIV1}/user/save`,
+    posts: `${APIV1}/posts`,
+    user: `${APIV1}/user/:id`,
+    dashboard: `${APIV1}/dashboard`,
+    menus: `${APIV1}/menus`,
+    v1test: `${APIV1}/test`,
+    v2test: `${APIV2}/test`,
+  },
+};
